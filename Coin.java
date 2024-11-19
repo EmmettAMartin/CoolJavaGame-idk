97e5b11d436ae8d926267b00a474ca14532aeade
Emmett <emmettmartin273@gmail.com>
2024-11-19 09:43:21 -0700

    Added functions for Coin and Enemy files.
    Added functions:
    In Coin ->
     - randomCoinCoordinate
     - isTouchingPlayer
    In Enemy ->
     - isTouchingPlayer
     - playerDirectionX
     - playerDirectionY
     - enemyDestination

--- Coin.java
+++ Coin.java
@@ -2,11 +2,20 @@ import java.util.Random;
 
 public class Coin {
   public int coinX, coinY;
-  static Random rand = new Random();
-  static int randomNum() {
-    return rand.nextInt(10);
-  }
-  // public regenerateCoinPositions() {
 
-  // }
+  static Random rand = new Random();
+
+  public int[] randomCoinCoordinate() {
+    int x = rand.nextInt(10);
+    int y = rand.nextInt(10);
+    int[] position = {x,y};
+    return position;
+  }
+
+  public boolean isTouchingPlayer(int playerX, int playerY, int coinX, int coinY) {
+    if ((coinX == playerX) && (coinY == playerY)) {
+      return true;
+    }
+    else { return false; }
+  }
 }
--- Enemy.java
+++ Enemy.java
@@ -1,9 +1,33 @@
 public class Enemy {
   int enemyX, enemyY;
 
-  public int player_direction_x(int playerX, int enemyX) {
+  public boolean isTouchingPlayer(int playerX, int playerY, int enemyX, int enemyY) {
+    if ((enemyX == playerX) && (enemyY == playerY)) {
+      return true;
+    }
+    else { return false; }
+  }
+
+  private static int playerDirectionX(int playerX, int enemyX) {
+    playerX += 1; // premature prevention of "division by 0" errors.
+    enemyX += 1;
     int x_diff = (playerX/enemyX)/Math.abs(playerX/enemyX);
     return x_diff;
   }
+
+  private static int playerDirectionY(int playerY, int enemyY) {
+    playerY += 1;
+    enemyY += 1;
+    int y_diff = (playerY/enemyY)/Math.abs(playerY/enemyY);
+    return y_diff;
+  }
   
+  public int[] enemyDestination(
+    int playerX, int playerY, int enemyX, int enemyY
+    ) {
+    enemyX += playerDirectionX(playerX, enemyX);
+    enemyY += playerDirectionY(playerY, enemyY);
+    int enemyNewPosition[] = {enemyX, enemyY};
+    return enemyNewPosition;
+  }
 }
