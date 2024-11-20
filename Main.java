0210fe02b161aadfcc2454250c52100f84a51f94
Emmett <emmettmartin273@gmail.com>
2024-11-20 00:34:03 -0700

    More WIP- the game is going to be window based.

--- Main.java
+++ Main.java
@@ -2,6 +2,22 @@ public class Main {
   static int enemyList[] = {};
   
   public static void main(String[] args) {
-
+    /*
+     * OK so
+     * the plan
+     * we have a loop that gets a character in
+     * then we handle said keypress
+     * if touching a coin -> collect it, add it to score, and spawn both an enemy and a coin
+     * if we are near coin -> give coin a value
+     * if we are touching an enemy -> kill player, end game
+     * then move the player
+     * then update all the enemies in the enemy list by iterating over it
+     * finally update and draw the map
+     * 
+     * CHANGE OF PLANS
+     * EVERYTHING IS ON A WINDOW NOW
+     * WHYYYYY
+     * 
+     */
   }
 }
--- Map.java
+++ Map.java
@@ -40,5 +40,4 @@ public class Map {
     givenMap[givenY][givenX] = value;
     return givenMap;
   }
-
 }
