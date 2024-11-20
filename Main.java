65ff709dc4f9c1ee0afe94f62c224e5e7811886b
Emmett <emmettmartin273@gmail.com>
2024-11-19 20:57:08 -0700

    WIP

--- Main.java
+++ Main.java
@@ -1,19 +1,7 @@
-/*
- * 
- * The structure of the program:
- * Everything is a class (waow it's Java crazy :O)
- * Coin- class with public int coinX, coinY -> public functions randomCoinCoordinate(), isTouchingCoin()
- * Enemy- another class. Every time we collect a coin, we spawn an enemy. When the enemy is touching a player, we kill the player.
- *    Public variables enemyX, enemyY, isTouchingPlayer()
- * Player- another class (NO REALLY). Public variables playerX, playerY, score, name. Public functions movePlayer()
- *    private/static functions are checkPlayerX(), checkPlayerY()
- * 
- */
-
 public class Main {
   static int enemyList[] = {};
   
   public static void main(String[] args) {
-    
+
   }
 }
